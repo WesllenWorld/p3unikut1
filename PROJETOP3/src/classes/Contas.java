9d1a60fe8567991457ef8bfd52cf21f294fe0882
Wesllen Leonardo de Lira Santos <71273407+WesllenWorld@users.noreply.github.com>
2021-04-03 16:36:13 -0300

    Update Contas.java

--- PROJETOP3/src/classes/Contas.java
+++ PROJETOP3/src/classes/Contas.java
@@ -6,26 +6,49 @@ import java.util.List;
 public class Contas {
     private List<Usuario> usuarios = new ArrayList();
 
-    public boolean cadastrarUsuario(Usuario u){//Método de inserção de um usuário
-        if(usuarios.contains(u)){
+    public boolean cadastrarUsuario(Usuario u) {//Método de inserção de um usuário
+        if (usuarios.contains(u)) {
             return false;
-        }else{
+        } else {
             usuarios.add(u);
             return true;
         }
     }
 
-    public boolean login(Usuario u){//Login
+    public boolean login(Usuario u) { //Login do usuário
 
-        for(int i = 0; i<usuarios.size();i++){
-            if(usuarios.get(i).getLogin().equals(u.getLogin())){
-                if(usuarios.get(i).getSenha().equals(u.getSenha())){
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getLogin().equals(u.getLogin())) {
+                if (usuarios.get(i).getSenha().equals(u.getSenha())) {
                     return true;
                 }
             }
         }
         return false;
     }
+
+    public void editarCadastro(Usuario u) {//Edição de perfil
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getLogin().equals(u.getLogin())) {
+                if (u.getSenha() == null) {//Editar apenas o nome
+                    usuarios.get(i).setNomeDeUsuario(u.getNomeDeUsuario());
+                    System.out.println(usuarios.get(i).toString());
+                    break;
+                } else if (u.getNomeDeUsuario() == null) {//Editar apenas a senha
+                    usuarios.get(i).setSenha(u.getSenha());
+                    System.out.println(usuarios.get(i).toString());
+                    break;
+                } else {//Editar nome e senha
+                    usuarios.set(i, u);
+                    System.out.println(usuarios.get(i).toString());
+                    break;
+                }
+
+            }
+        }
+    }
+
 
     //Demais métodos
 
