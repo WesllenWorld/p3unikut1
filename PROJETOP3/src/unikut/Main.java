4f2cf4c9af6443193a4daae4d0a9be8583df689b
Wesllen Leonardo de Lira Santos <71273407+WesllenWorld@users.noreply.github.com>
2021-04-03 15:11:06 -0300

    Update Main.java

--- PROJETOP3/src/unikut/Main.java
+++ PROJETOP3/src/unikut/Main.java
@@ -4,12 +4,13 @@
     Flávio Antônio Medeiros de Farias
     Ryan Pereira Lima da Silva
     Wesllen Leonardo de Lira Santos
-
 */
 package unikut;
+
 import classes.*;
 
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,6 +19,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String nome, login, senha;
         int op, acaoDeConta;
+        boolean logado;
 
         do {
             menuInicial();
@@ -35,17 +37,17 @@ public class Main {
                     login = in.next();
                     System.out.println("Senha do usuário:");
                     senha = in.next();
-                    System.out.println("Nome de usuário (opcional. Seu nome será visto como "+"\""+"convidado"+"\""+" caso não preencha o espaço.");
+                    System.out.println("Nome de usuário (opcional. Seu nome será visto como " + "\"" + "convidado" + "\"" + " caso não preencha o espaço.");
                     in.nextLine();
                     nome = in.nextLine();
-                    if(nome.equals("")){
+                    if (nome.equals("")) {
                         nome = "Convidado";
                     }
                     us = new Usuario(login, senha, nome);
                     boolean cadastroEfetivado = social.cadastrarUsuario(us);
-                    if(cadastroEfetivado){
+                    if (cadastroEfetivado) {
                         System.out.println("Cadastro concluído com sucesso.");
-                    }else{
+                    } else {
                         System.out.println("Não foi possível efetivar seu cadastro pois o nome de usuário já está em uso.");
                     }
                     break;
@@ -56,14 +58,20 @@ public class Main {
                     login = in.next();
                     System.out.println("Senha: ");
                     senha = in.next();
-                    
-                    do{
-                        menuDeConta();
-                        acaoDeConta = in.nextInt();
+
+                    us = new Usuario(login, senha, "");
+                    logado = social.login(us);
+                    if (logado) {
+                        System.out.println("Bem-vindo, " + us.getNomeDeUsuario() + "!");
+                        do {
+                            menuDeConta();
+                            acaoDeConta = in.nextInt();
 
 
-
-                    }while(acaoDeConta != 0);
+                        } while (acaoDeConta != 0);
+                    } else {
+                        System.out.println("Login ou senha inválidos. Tente novamente.");
+                    }
 
                     break;
             }
@@ -71,18 +79,21 @@ public class Main {
         } while (op != 0);
 
 
-
     }
+
     public static void menuInicial() {//Menu inicial 
-        System.out.println("BEM VINDO AO UNIKUT, A MAIS NOVA FORMA DE MANTER A UNICAP MAIS PERTINHO DE VOCÊ.");
+        System.out.println("BEM-VINDO AO UNIKUT, A MAIS NOVA FORMA DE MANTER A UNICAP MAIS PERTINHO DE VOCÊ.");
 
         System.out.println("Tecle - 1 – Para fazer parte da comunidade do UNIKUT.");
         System.out.println("Tecle - 2 – Para logar agora mesmo, caso já faça parte da comunidade.");
         System.out.println("Tecle 0 – para sair do programa.");
     }
 
-    public static void menuDeConta(){//Menu do usuário logado
-
+    public static void menuDeConta() {//Menu do usuário logado
+        System.out.println("Funções disponíveis:");
+        System.out.println("1 – Editar perfil.");
+        System.out.println("2 – Adicionar um amigo.");
+        System.out.println("3 - Enviar um recado.");
     }
 
 
