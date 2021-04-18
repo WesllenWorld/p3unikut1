26c259c4b0ad6c1cc177df3f849d8d0d0dde834b
aviladiogo <66319119+aviladiogo@users.noreply.github.com>
2021-04-17 22:04:13 -0300

    Update attUsuarios.java
    Resolução Prova 1 Questão D (Classe Usuário)

--- PROJETOP3/src/usuarios/attUsuarios.java
+++ PROJETOP3/src/usuarios/attUsuarios.java
@@ -17,8 +17,100 @@ public class Usuario {
         recados = new ArrayList();
         murais = new ArrayList();
     }
-  
-  public String listaMurais() {//Exibir murais
+
+    public String getLogin() {
+
+        return login;
+    }
+
+    public void setNomeDeUsuario(String nomeDeUsuario) {
+        this.nomeDeUsuario = nomeDeUsuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeDeUsuario() {
+
+        return nomeDeUsuario;
+    }
+
+    public String getSenha() {
+
+        return senha;
+    }
+
+    public int adicionar(Usuario u) {//Adicionar amigos
+        if (this.pendentes.contains(u)) {//Se já existe um convite pendente de "u"
+            this.amigos.add(u);
+            u.amigos.add(this);
+            pendentes.remove(u);
+            u.pendentes.remove(this);
+            return 1;//1 corresponde à aceitação do pedido de amizade
+        } else if (u.pendentes.contains(this)) {//Se "u" possui um convite do usuário
+            return -1;//falha
+        } else if (u.amigos.contains(this) && this.amigos.contains(u)) {//Se ambos são amigos
+            return -1;//falha
+        } else {
+            u.pendentes.add(this);
+        }
+        return 0;//0 corresponde ao envio do pedido, permanecendo como 'pendente'
+    }
+
+    public void adicionarRecado(String recado) {
+        recados.add(recado);
+    }
+
+    public boolean limparRecados() {
+        if (recados.size() == 0) {
+            return false;
+        } else {
+            recados.clear();
+            return true;
+        }
+    }
+
+    @Override //Comparador de usuários utilizando equals com o login
+    public boolean equals(Object o) {
+        Usuario u = (Usuario) o;
+        if (this.login.equals(u.login)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getLogin() + "\n";
+    }
+
+    public String listaDeAmigos() {//Exibir amigos
+        String list = "";
+        for (Usuario u : amigos) {
+            list += u.toString();
+        }
+        return list;
+    }
+
+    public String listaDePendentes() {//Exibir pendentes
+        String list = "";
+        for (Usuario u : pendentes) {
+            list += u.toString();
+        }
+        return list;
+    }
+
+    public String listaRecados() {//Exibir recados
+        String list = "";
+        for (int i = 0; i < recados.size(); i++) {
+            list += recados.get(i) + "\n";
+        }
+        return list;
+    }
+
+    public String listaMurais() {//Exibir murais
         String list = "";
         for (int i = 0; i < murais.size(); i++) {
             list += murais.get(i) + "\n";
@@ -40,5 +132,3 @@ public class Usuario {
         }
     }
 }
-  
-    
