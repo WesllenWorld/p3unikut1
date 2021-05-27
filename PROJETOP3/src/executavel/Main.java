f42bf392da9c10b858cab1f3106dbb0fbcddab52
aviladiogo <66319119+aviladiogo@users.noreply.github.com>
2021-05-27 15:24:51 -0300

    Atualização do programa
    atualizando manualmente por problemas com o gitcommit
    Adicionando threads

---
+++ PROJETOP3/src/executavel/Main.java
@@ -0,0 +1,10 @@
+package executavel;
+
+import view.preLoginAutenticado.Home;
+
+public class Main {
+    public static void main(String[] args) {
+        Home a = new Home();
+        a.home();
+    }
+}
